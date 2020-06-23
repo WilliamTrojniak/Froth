6a1b935f365d6165382c18d57655b541091c2e97
William Trojniak <william.trojniak@gmail.com>
2020-06-23 00:02:16 -0400

    Added "Cream" to workspace and linked to "Playground"

--- premake5.lua
+++ premake5.lua
@@ -1,27 +1,97 @@
 -- premake5.lua
 workspace "Cream"
     configurations { "Debug", "Release", "Distribution"}
+    platforms { "Win64" }
+
+  filter "configurations:Debug"
+		defines {"CREAM_DEBUG"}
+		symbols "on"
+
+	filter "configurations:Release"
+    defines {"CREAM_RELEASE"}
+		optimize "on"
+
+	filter "configurations:Distribution"
+    defines {"CREAM_DISTRIBUTION"}
+    optimize "on"
+    
+  filter { "platforms:Win64" }
+    systemversion "latest"
+    system "Windows"
+    architecture "x86_64"
+
+  filter {}
 
 outputDirectory = "%{cfg.system}%{cfg.buildcfg}-%{cfg.architecture}/%{prj.name}"
+
+project "Cream"
+  location "Cream"
+  kind "StaticLib"
+  language "C++"
+  cppdialect "C++17"
+  staticruntime "on"
+
+  targetdir ("bin/"..outputDirectory)
+  objdir ("bin-int/"..outputDirectory)
+
+  pchheader "creampch.h"
+  pchsource "Cream/_src/creampch.cpp"
+
+  files {
+		"%{prj.name}/_src/**.h",
+		"%{prj.name}/_src/**.cpp"
+  }
+
+  includedirs {
+    "%{prj.name}/src"
+  }
+
+  links {
+
+  }
+
+  filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Distribution"
+		runtime "Release"
+		optimize "on"
+
+  filter {}
 
 project "Playground"
     location "Playground"
     kind "ConsoleApp"
     language "C++"
     cppdialect "C++17"
+    staticruntime "on"
 
     targetdir ("bin/"..outputDirectory)
 
-    files
-	{
+    files {
 		"%{prj.name}/_src/**.h",
 		"%{prj.name}/_src/**.cpp"
     }
-    
-    filter "configurations:Debug"
-      defines { "DEBUG" }
-      symbols "On"
 
-   filter "configurations:Release"
-      defines { "NDEBUG" }
-      optimize "On"
+    links {
+      "Cream"
+    }
+    
+  filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Distribution"
+		runtime "Release"
+    optimize "on"
+    
+  filter {}

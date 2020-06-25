3beadfd6aac9cd20562ff247552a37a45e984952
William Trojniak <william.trojniak@gmail.com>
2020-06-25 17:04:46 -0400

    Test for proper Vulkan installation

--- Playground/_src/Playground.cpp
+++ Playground/_src/Playground.cpp
@@ -1,12 +1,44 @@
 #include "Cream/Core/Entrypoint.h"
 #include <iostream>
 
+//Temporary
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <iostream>
+
 class Playground : public Cream::Application
 {
 public:
 	Playground()
 	{
-		std::cout << "Playground created!" << std::endl;
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+        std::cout << extensionCount << " extensions supported\n";
+
+        glm::mat4 matrix;
+        glm::vec4 vec;
+        auto test = matrix * vec;
+
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+        }
+
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
 	}
 };
 

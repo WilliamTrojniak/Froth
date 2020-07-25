cf3a3e7ebeb16bbf90f3e377f3dee2c3e5a48e85
William Trojniak <william.trojniak@gmail.com>
2020-07-25 15:52:33 -0400

    Single include header file

--- Cream/_src/Cream/Core/Application.h
+++ Cream/_src/Cream/Core/Application.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "Cream/Utility/Utility.h"
+#include "Base.h"
 
 namespace Cream
 {
---
+++ Cream/_src/Cream/Core/Base.h
@@ -0,0 +1,9 @@
+#pragma once
+// Platform Independent Includes
+#include "Cream/Platform Dependent/BuildDefines.h"
+#include "Cream/Platform Dependent/PrimitiveTypes.h"
+#include "Cream/Platform Dependent/Collections/SmartPointers.h"
+
+// Core Systems
+#include "Cream/Core/Assertions.h"
+#include "Cream/Core/Memory/StackAllocator.h"

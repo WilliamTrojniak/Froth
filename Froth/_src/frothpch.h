4a8b2c11b848f574b53911c1f1935c7c79108109
William Trojniak <william.trojniak@gmail.com>
2021-01-30 12:28:18 -0500

    Updated

--- Froth/_src/frothpch.h
+++ Froth/_src/frothpch.h
@@ -1,4 +1,8 @@
 #pragma once
 #include <iostream>
+#include <string>
+#include <vector>
+#include <chrono>
 
 #include "Froth/Core Layer/Assertions.h"
+#include "Froth/Platform Independence Layer/PrimitiveTypes.h"

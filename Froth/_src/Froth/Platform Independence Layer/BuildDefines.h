74d0240df4e769fbacfbcbd2495ae453f9cbe75d
William Trojniak <william.trojniak@gmail.com>
2021-09-12 21:11:57 -0400

    Added platform dependent and misc. defines

--- Froth/_src/Froth/Platform Independence Layer/BuildDefines.h
+++ Froth/_src/Froth/Platform Independence Layer/BuildDefines.h
@@ -1,8 +1,18 @@
 #pragma once
+#define STB_IMAGE_IMPLEMENTATION
+
+
+// General Defines
+#define FROTH_ENGINE_NAME "FROTH"
+#define FROTH_VERSION_MAJOR 0
+#define FROTH_VERSION_MINOR 0
+#define FROTH_VERSION_REVISION 1
+
 
 // Debugging specific defines
 #if defined(FROTH_DEBUG)
 #define FROTH_ENABLE_ASSERTIONS
+#define FROTH_ENABLE_VKVALIDATIONLAYER
 
 #elif defined(FROTH_RELEASE)
 
@@ -14,4 +24,7 @@
 
 #if defined(FROTH_PLATFORM_WINDOWS)
 #define FROTH_DEBUGBREAK() __debugbreak()
+#define FROTH_WINDOWAPI_WIN
+#define VK_USE_PLATFORM_WIN32_KHR
+#define FROTH_GRAPHICSAPI_VK
 #endif

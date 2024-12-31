d76f2e082bc033245b36fc53268953f7b1c8b389
Will Trojniak <william.trojniak@gmail.com>
2024-12-31 08:11:18 -0500

    GLFW and Vulkan to create window

--- froth/core/Application.cpp
+++ froth/core/Application.cpp
@@ -1,7 +1,23 @@
+#define GLFW_INCLUDE_VULKAN
 #include "core/Application.h"
+#include <GLFW/glfw3.h>
 
 namespace Froth {
-Application::Application() {}
+Application::Application() {
+
+  if (!glfwInit()) {
+  }
+  GLFWwindow *window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  if (window) {
+    glfwMakeContextCurrent(window);
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+  }
+
+  glfwTerminate();
+}
 Application::~Application() {}
 
 void Application::Run() {}

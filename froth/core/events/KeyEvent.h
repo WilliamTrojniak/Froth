a94c1933824489fa2e4056f3497bc84adee8a156
Will Trojniak <william.trojniak@gmail.com>
2025-01-02 10:44:50 -0700

    Change to use KeyCode type

--- froth/core/events/KeyEvent.h
+++ froth/core/events/KeyEvent.h
@@ -2,21 +2,20 @@
 
 #include "core/events/Event.h"
 #include "platform/keys/Keycodes.h"
-#include <cstdint>
 
 namespace Froth {
 class KeyEvent : public Event {
 private:
-  uint32_t m_Keycode;
+  KeyCode m_Keycode;
 
 public:
-  inline uint32_t keyCode() const { return m_Keycode; }
+  inline KeyCode keyCode() const { return m_Keycode; }
   virtual inline int categoryFlags() const override {
     return EventCategoryInput | EventCategoryKeyboard;
   }
 
 protected:
-  KeyEvent(uint32_t keycode) : m_Keycode(keycode) {}
+  KeyEvent(KeyCode keycode) : m_Keycode(keycode) {}
 };
 
 class KeyPressedEvent : public KeyEvent {
@@ -24,7 +23,7 @@ private:
   bool m_IsRepeat;
 
 public:
-  KeyPressedEvent(uint32_t keycode, bool isRepeat)
+  KeyPressedEvent(KeyCode keycode, bool isRepeat)
       : KeyEvent(keycode), m_IsRepeat(isRepeat) {}
 
   inline bool isRepeat() const { return m_IsRepeat; }
